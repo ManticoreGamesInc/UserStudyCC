d80bdba36920ed89215283a71e53ebece4a86f49
standardcombo <standardcombo@gmail.com>
2021-06-22 11:56:00 -0700

    Chat commands now wrapped with a Task.Spawned because hooks don't allow thread yielding inside them.

--- Data/Scripts/UserStudyChatCommands.lua
+++ Data/Scripts/UserStudyChatCommands.lua
@@ -29,18 +29,23 @@ function OnReceiveMessageHook(player, params)
 	local command, arg1, arg2, arg3, arg4 = CoreString.Split(message)
 	local arguments = {arg1, arg2, arg3, arg4}
 	
-	if command == "/study" then
-		API.BeginStudy(player, arguments)
+	-- We need to spawn task because yielding the thread is not allowed inside a hook
+	Task.Spawn(function()
+		if not Object.IsValid(player) then return end
 		
-	elseif command == "/endstudy" then
-		API.EndStudy(player, arguments)
-		
-	elseif command == "/next" then
-		API.NextSubject(player)
-		
-	elseif command == "/prev" then
-		API.PreviousSubject(player)
-	end
+		if command == "/study" then
+			API.BeginStudy(player, arguments)
+			
+		elseif command == "/endstudy" then
+			API.EndStudy(player, arguments)
+			
+		elseif command == "/next" then
+			API.NextSubject(player)
+			
+		elseif command == "/prev" then
+			API.PreviousSubject(player)
+		end
+	end)
 end
 
 Chat.receiveMessageHook:Connect(OnReceiveMessageHook)
